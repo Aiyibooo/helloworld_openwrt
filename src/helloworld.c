fd244ed4aeb577b006e4689232db6a458c202633
Louis <wangyibocn@163.com>
2022-02-14 13:39:36 +0800

    first version

---
+++ src/helloworld.c
@@ -0,0 +1,23 @@
+/* ************************************************************************
+ *       Filename:  helloworld.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年01月22日 15时25分05秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, world!\n");
+
+    return 0;
+}
+
+
+
